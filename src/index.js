d126bbd87a7d708172c2db8fb881c5de7845022a
Aman Dua -X (amdua - ALTRAN ACT at Cisco) <amdua@cisco.com>
2020-12-11 15:43:32 +0530

    basic calculator app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './com';
+
+import Calculator from  './componets/Calculator'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
